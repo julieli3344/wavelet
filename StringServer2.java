8b1fbbdb2137c243876fa195b3a8135554c97dad
julieli334433 <jul086@ucsd.edu>
2023-04-24 22:46:54 -0700

    updated

--- StringServer2.java
+++ StringServer2.java
@@ -2,20 +2,22 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a string that will be manipulated by
     // various requests.
     String message = "";
-    Set<String> messages = new HashSet<>(); // Keep track of unique messages
+    ArrayList<String> lists = new ArrayList<>();
+    // Set<String> messages = new HashSet<>(); // Keep track of unique messages
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/add-message")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 String newMessage = parameters[1];
-                if (!messages.contains(newMessage)) { // Check if message already exists
-                    messages.add(newMessage);
+                if (!message.contains(newMessage)) { // Check if message already exists
+                    lists.add(newMessage);
                     message += "\n" + newMessage; // Append the new message to the existing string with a new line character
                 }
             }
