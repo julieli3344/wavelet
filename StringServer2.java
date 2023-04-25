61c5a3293fe1353fad67f2fbb848bba34fa2dca6
julieli334433 <jul086@ucsd.edu>
2023-04-24 22:43:53 -0700

    updated

--- StringServer2.java
+++ StringServer2.java
@@ -1,34 +1,27 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a string that will be manipulated by
     // various requests.
     String message = "";
+    Set<String> messages = new HashSet<>(); // Keep track of unique messages
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/add-message")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
-                message += "\n" + parameters[1]; // Append the new message to the existing string with a new line character
+                String newMessage = parameters[1];
+                if (!messages.contains(newMessage)) { // Check if message already exists
+                    messages.add(newMessage);
+                    message += "\n" + newMessage; // Append the new message to the existing string with a new line character
+                }
             }
             return message;
         } else {
             return "404 Not Found!";
         }
-    }
-}
-
-class StringServer2 {
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
-        }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
     }
 }
